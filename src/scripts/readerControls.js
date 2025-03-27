a16ba1cf677da1b08ca4a8127120c4a35e6cfc00
skolomorphic <skolomorphic@protonmail.com>
2025-03-27 12:04:58 -0700

    refactor reader controls; improve comment view transitions

--- src/scripts/readerControls.js
+++ src/scripts/readerControls.js
@@ -1,52 +1,62 @@
 function changeProperty(property, changeValue, measure) {
     let docElement = document.getElementById('book-content');
     let oldValue = window.getComputedStyle(docElement).getPropertyValue(property);
+    console.log('oldValue', oldValue);
     let newValue = measure ? parseFloat(oldValue) + changeValue + measure : changeValue;
     docElement.style.setProperty(property, newValue);
     localStorage.setItem(property, newValue);
 }
 
 document.querySelector('.decreaseFont').addEventListener('click', () => {
-    changeProperty('font-size', -2, 'px');
+    changeProperty('--reader-font-size', -2, 'px');
 });
 
 document.querySelector('.increaseFont').addEventListener('click', () => {
-    changeProperty('font-size', 2, 'px');
+    changeProperty('--reader-font-size', 2, 'px');
 });
 
 document.querySelector('.decreaseLineHeight').addEventListener('click', () => {
-    changeProperty('line-height', -2, 'px');
+    changeProperty('--reader-line-height', -2, 'px');
 });
 
 document.querySelector('.increaseLineHeight').addEventListener('click', () => {
-    changeProperty('line-height', 2, 'px');
+    changeProperty('--reader-line-height', 2, 'px');
+});
+
+document.querySelector('.decreasePSpacing').addEventListener('click', () => {
+    changeProperty('--reader-p-spacing', -5, 'px');
+});
+
+document.querySelector('.increasePSpacing').addEventListener('click', () => {
+    changeProperty('--reader-p-spacing', 5, 'px');
 });
 
 document.querySelector('.changeArial').addEventListener('click', () => {
-    changeProperty('font-family', "Arial, sans-serif", '');
+    changeProperty('--reader-font-family', "Arial, sans-serif", '');
 });
 
 document.querySelector('.changeGeorgia').addEventListener('click', () => {
-    changeProperty('font-family', "Georgia, serif", '');
+    changeProperty('--reader-font-family', "Georgia, serif", '');
 });
 
 document.querySelector('.changeHelvetica').addEventListener('click', () => {
-    changeProperty('font-family', "Helvetica, sans-serif", '');
+    changeProperty('--reader-font-family', "Helvetica, sans-serif", '');
 });
 
 document.querySelector('.changeTrebuchet').addEventListener('click', () => {
-    changeProperty('font-family', "'Trebuchet MS', sans-serif", '');
+    changeProperty('--reader-font-family', "'Trebuchet MS', sans-serif", '');
 });
 
 document.querySelector('.changeTimes').addEventListener('click', () => {
-    changeProperty('font-family', "'Times New Roman', serif", '');
+    changeProperty('--reader-font-family', "'Times New Roman', serif", '');
 });
 
 document.querySelector('.restoreDefaults').addEventListener('click', () => {
     let docElement = document.getElementById('book-content');
-    docElement.style.removeProperty('font-family');
-    docElement.style.removeProperty('font-size');
-    docElement.style.removeProperty('line-height');
+    docElement.style.setProperty('--reader-font-family', docElement.style.getPropertyValue('--default-font-family'));
+    docElement.style.setProperty('--reader-font-size', docElement.style.getPropertyValue('--default-font-size'));
+    docElement.style.setProperty('--reader-line-height', docElement.style.getPropertyValue('--default-line-height'));
+    docElement.style.setProperty('--reader-p-spacing', docElement.style.getPropertyValue('--default-p-spacing'));
     localStorage.clear();
 });
 
